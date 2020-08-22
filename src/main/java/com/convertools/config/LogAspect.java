dcb9c656ad7c02c62a7c6b0b10c046c692bde4be
fangkun <kun_ja@163.com>
2020-08-22 11:14:04 +0800

    11

---
+++ src/main/java/com/convertools/config/LogAspect.java
@@ -0,0 +1,35 @@
+package com.convertools.config;
+
+import com.convertools.service.MdbService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Aspect
+public class LogAspect {
+
+    private static Log logger = LogFactory.getLog(LogAspect.class);
+
+    @Pointcut("within(com.convertools.service.*)")
+    public void pointCut() {
+
+    }
+
+    @Before("pointCut()")
+    public void beforePointCut(JoinPoint point) {
+        logger.info("exec before .." + point.getSignature());
+        System.out.println(point);
+    }
+
+    @After("pointCut()")
+    public void afterPointCut(JoinPoint point) {
+        logger.info("exec after .." + point.getSignature());
+        System.out.println(point);
+    }
+}
