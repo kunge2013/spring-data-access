32dd4ae6d1189fd753f3087c2dbdcb877eb2c608
fangkun <kun_ja@163.com>
2021-04-24 20:48:16 +0800

    单机本稳定

--- src/main/java/com/convertools/handler/DataPostHandler.java
+++ src/main/java/com/convertools/handler/DataPostHandler.java
@@ -1,10 +1,12 @@
 package com.convertools.handler;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
+import java.util.Set;
 
 /**
  * @author fangkun
@@ -17,10 +19,20 @@ public class DataPostHandler {
     @Autowired
     ApplicationContext context;
 
+    @Value("${exec:false}")
+    private boolean execPostApi;
+
     void handler(String fileName) {
         Map<String, UploadService> beansOfType = context.getBeansOfType(UploadService.class);
-        for (UploadService service : beansOfType.values()) {
-            service.callHttpExt(fileName);
+        Set<Map.Entry<String, UploadService>> entries = beansOfType.entrySet();
+        for (Map.Entry<String, UploadService> entry : entries) {
+            String key = entry.getKey();
+            if ("uploadServiceImpl".equalsIgnoreCase(key)) {
+                entry.getValue().callHttpExt(fileName);
+            }
+            if (!"uploadServiceImpl".equalsIgnoreCase(key) && execPostApi) {
+                entry.getValue().callHttpExt(fileName);
+            }
         }
     }
 }
